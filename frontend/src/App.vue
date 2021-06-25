1ef8017898d769a4f8aecbca31d1274ac84dfdd4
崮生 <admin@shenzilong.cn>
2021-06-25 18:39:03 +0900

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <n-button>naive-ui</n-button>
+    <div class="text-dark-50">333</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { NButton } from 'naive-ui'
+export default defineComponent({
+  name: 'App',
+  components: {
+    NButton
+  }
+})
+</script>
+
+<style>
+
+</style>
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,14 @@
+// tailwind.config.ts
+import { defineConfig } from 'windicss/helpers'
+import formsPlugin from 'windicss/plugin/forms'
+
+export default defineConfig({
+  darkMode: 'class',
+  safelist: 'p-3 p-4 p-5',
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [formsPlugin],
+})
